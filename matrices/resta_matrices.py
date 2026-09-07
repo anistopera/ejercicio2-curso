21749646d58f491d8ac7938c6c38fd2c13aeae21
darlin rosse <202401540est.umss.edu>
2026-09-07 14:59:18 -0400

    feat(eq02): [HU201] agrega manejo de matrices vacias o no validas

--- matrices/resta_matrices.py
+++ matrices/resta_matrices.py
@@ -5,7 +5,9 @@ Encargado: [Persona F]
 
 def resta_matrices(m1: list, m2: list) -> list:
     """Resta elemento a elemento entre dos matrices (listas de listas)."""
-    if  len(m1) != len(m2) or len(m1[0] != len(m2[0])):
+    if not m1 or not m2:
+        raise ValueError("Las matrices no pueden estar vacias, ingrese matrices validas ")
+    if  len(m1) != len(m2) or len(m1[0]) != len(m2[0]):
         raise ValueError("Las matrices deben tener el mismo tamaño para poder restarlas ")
 
     resultado = []
@@ -15,3 +17,5 @@ def resta_matrices(m1: list, m2: list) -> list:
             fila.append(m1[i][j] - m2[i][j])
         resultado.append(fila)
     return resultado
+
+    
