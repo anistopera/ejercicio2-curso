b46ac4ee2a8ca5468751cd7e84571180adf16c6c
darlin rosse <202401540est.umss.edu>
2026-09-07 14:37:12 -0400

    feat(eq02): [HU201] agrega validacion de tamaño en resta_matrices

--- matrices/resta_matrices.py
+++ matrices/resta_matrices.py
@@ -5,5 +5,6 @@ Encargado: [Persona F]
 
 def resta_matrices(m1: list, m2: list) -> list:
     """Resta elemento a elemento entre dos matrices (listas de listas)."""
-    # TODO: implementar
-    pass
+    if  len(m1) != len(m2) or len(m1[0] != len(m2[0])):
+        raise ValueError("Las matrices deben tener el mismo tamaño para poder restarlas ")
+
