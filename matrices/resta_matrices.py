442f41113fa5cd1a5059872cf97ec0d36a5cdf01
darlin rosse <202401540est.umss.edu>
2026-09-07 14:49:27 -0400

    feat(eq02): [HU201] agrega logica de resta entre matrices

--- matrices/resta_matrices.py
+++ matrices/resta_matrices.py
@@ -8,3 +8,10 @@ def resta_matrices(m1: list, m2: list) -> list:
     if  len(m1) != len(m2) or len(m1[0] != len(m2[0])):
         raise ValueError("Las matrices deben tener el mismo tamaño para poder restarlas ")
 
+    resultado = []
+    for i in range(len(m1)):
+        fila =[]
+        for j in range(len(m1[0])):
+            fila.append(m1[i][j] - m2[i][j])
+        resultado.append(fila)
+    return resultado
