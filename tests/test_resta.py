17c0268c49684d6032650ef6f95043df538d301a
jhosiasdaza-bit <jhosiasdaza@gmail.com>
2026-09-07 22:14:03 -0400

    fix(eq02): [HU-201] corrige imports absolutos a relativos en tests

--- tests/test_resta.py
+++ tests/test_resta.py
@@ -1,13 +1,13 @@
 import unittest
 
 
-from enteros.resta_enteros import resta_enteros
-from decimales.resta_decimales import resta_decimales
-from negativos.resta_negativos import resta_negativos
-from lista.resta_lista import resta_lista
-from entre_listas.resta_entre_listas import resta_entre_listas
-from matrices.resta_matrices import resta_matrices
-from validacion.validador import validar_numero, validar_lista_numerica
+from ..enteros.resta_enteros import resta_enteros
+from ..decimales.resta_decimales import resta_decimales
+from ..negativos.resta_negativos import resta_negativos
+from ..lista.resta_lista import resta_lista
+from ..entre_listas.resta_entre_listas import resta_entre_listas
+from ..matrices.resta_matrices import resta_matrices
+from ..validacion.validador import validar_numero, validar_lista_numerica
 
 class TestRestaBugHunters(unittest.TestCase):
 
